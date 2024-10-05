0dcca7d5f58c3f0c4b105fb88a5aeba9ce650e2c
Hoony <kkhkkh7739@gmail.com>
2024-10-05 20:27:08 +0900

    Test

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // app 폴더 경로 추가
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
