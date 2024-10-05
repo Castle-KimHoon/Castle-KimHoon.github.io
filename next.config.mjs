f6383ea82e677f71dc52275d47882798d555284c
Hoony <kkhkkh7739@gmail.com>
2024-10-05 20:50:25 +0900

    T1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/castle-kimhoon.github.io', // GitHub Pages의 기본 경로 설정
-  assetPrefix: '/castle-kimhoon.github.io/', // 정적 파일의 경로 설정
 };
 
 export default nextConfig;
