dc1c62f4877d1f2a988c45f2fa59d98030a3d7ae
Hoony <kkhkkh7739@gmail.com>
2024-10-12 15:41:28 +0900

    T2

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ npm run build
 npm run export
 
 # public 폴더 생성 및 복사
-mkdir -p public
-cp -r out/* public/
+mkdir -p Blog
+cp -r out/* Blog/
 
 # out 폴더 삭제
 rm -rf out
 
 # Git에 추가 및 커밋
-git add public/*
-git commit -m "Deploy new version"
+git add Blog/*
+git commit -m "Blog new version"
 
 # 원격 저장소에 푸시
 git push origin main
